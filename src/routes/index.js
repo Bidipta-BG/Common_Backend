ce68442f9c77a454b821539bae0669a26d31b82b
Bidipta Gogoi <gogoibidipta@gmail.com>
2026-03-15 00:14:20 +0530

    ai-analyzer-commented

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const greetingRoutes = require('../apps/greeting-app/routes');
 const axomitlabRoutes = require('../apps/axomitlab/routes');
 const srikrishnaAartiRoutes = require('../apps/srikrishna-aarti/routes');
-const aiBusinessAnalyzerRoutes = require('../apps/ai-business-analyzer');
+// const aiBusinessAnalyzerRoutes = require('../apps/ai-business-analyzer');
 const shivjiPujaRoutes = require('../apps/shivji-puja/routes');
 const hanumanjiPujaRoutes = require('../apps/hanumanji-puja/routes');
 
@@ -11,7 +11,7 @@ router.get('/health', (req, res) => res.json({ status: 'up' }));
 router.use('/greeting-app', greetingRoutes);
 router.use('/axomitlab', axomitlabRoutes);
 router.use('/srikrishna-aarti', srikrishnaAartiRoutes);
-router.use('/ai-business-analyzer', aiBusinessAnalyzerRoutes);
+// router.use('/ai-business-analyzer', aiBusinessAnalyzerRoutes);
 router.use('/shivji-puja', shivjiPujaRoutes);
 router.use('/hanumanji-puja', hanumanjiPujaRoutes);
 
